8a07adf0398af470023d1af64d3f0319e83018d4
PredragKostic <pedja.kostic@hotmail.com>
2019-07-09 21:56:51 +0200

    fetch posts

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 require 'classes/Database.php';
 
-$datebase = new Database;
+$database = new Database;
+
+$database->query('SELECT * FROM posts WHERE id = :id');
+$database->bind(':id', 1);
+
+$rows = $database->resultset();
+?>
+
+<h1>Posta</h1>
+<?php foreach($rows as $row){ ?>
+    <h3><?= $row['id']; ?></h3>
+    <p><?php echo $row['body']; ?></p>
+<?php } ?>
